56bf90dbd30bf78da4035279417e0900b7bd3c82
gregorius2514 <gregorius2514@wp.pl>
2014-08-05 14:38:18 +0200

    add aplication icon.

--- sample/Main.java
+++ sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,6 +15,7 @@ public class Main extends Application {
 
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Klient Refleksji");
+        primaryStage.getIcons().add(new Image("resources/icon.png"));
 
         scene = new Scene(root, 600, 375);
         primaryStage.setScene(scene);
